6765ebb89deb1af278b9f06765a0a0c8febecff9
lisi <lisi@itcast.cn>
2019-06-23 16:48:59 +0800

    修改文件

--- gitTest1/src/com/itheima/git/GitTest1.java
+++ gitTest1/src/com/itheima/git/GitTest1.java
@@ -6,5 +6,7 @@ public class GitTest1 {
         System.out.println("111111");
         System.out.println("222222");
         System.out.println("333333");
+        System.out.println("444444");
+        System.out.println("555555");
     }
 }
