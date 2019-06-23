4e0e805452f16d7cefeecc07ec15d2caf6615a3b
lisi <lisi@itcast.cn>
2019-06-23 16:38:48 +0800

    上传GitTest文件

---
+++ gitTest1/src/com/itheima/git/GitTest1.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+public class GitTest1 {
+
+    public static void main(String[] args) {
+        System.out.println("111111");
+        System.out.println("222222");
+        System.out.println("333333");
+    }
+}
